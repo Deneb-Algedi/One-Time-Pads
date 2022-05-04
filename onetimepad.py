c41e28c8343fd5be46e702ac258bcc1c89c39b62
Deneb-Algedi <46696752+Deneb-Algedi@users.noreply.github.com>
2022-05-04 14:53:02 -0700

    Updated it to python3

--- onetimepad.py
+++ onetimepad.py
@@ -1,17 +1,21 @@
-# This function performs xor on hex strings
-def xor2str(C1, C2):
-	C1 = C1.decode('hex')
-	C2 = C2.decode('hex')
+import codecs
 
+decode_hex = codecs.getdecoder("hex_codec")
+
+"""This function performs xor on two strings"""
+def xor2str(C1, C2):
+	C1 = decode_hex(C1)[0]
+	C2 = decode_hex(C2)[0]
+	
 	result = ""
 	for c1, c2 in zip(C1, C2):
-		result += chr(ord(c1)^ord(c2))
-	return result.encode("hex")
+		result += chr((c1)^(c2))
+	
+	return(result.encode("utf-8").hex())
 
 def main():
-
 	# This is the message I created and fed to the application
-	MSG = "I love her so much I could die!!".encode("hex")
+	MSG = "I love her so much I could die!!".encode("utf-8").hex()
 	# This is the encrypted message given by the application
 	C1 = "928f973ee2d5967d928462ed119eb5e15fa49a61e144f4335778c162b998748e"
 	# This is my message encrypted by the application
@@ -21,7 +25,7 @@ def main():
 	# Performing xor on the resulting hex with the message
 	xoredMsg = xor2str(xoredCiphers, MSG)
 	# display decrypted message
-	print(xoredMsg.decode('hex'))
+	print(str(decode_hex(xoredMsg)[0], 'utf-8')) # different way to perform same operation print(str(codecs.decode(bytes(xoredMsg, encoding='utf-8'), "hex"),'utf-8'))
 
 if __name__ == "__main__":
     main()
